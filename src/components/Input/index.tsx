3921d53baa66a47d6c10f8d6334accb6bc531b57
renan-martini <renan.martiniduarte@gmail.com>
2022-11-11 11:43:37 -0300

    feat: projeto concluido

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,41 @@
+import { StyledDivInput } from "./styles";
+import image from "../../assets/Ellipse1.png";
+import { useContext, useState } from "react";
+import { VideoContext } from "../../providers/video";
+import API from "../../services/api";
+import { toast } from "react-toastify";
+
+function Input() {
+  const { setVideo, setVideoUrl } = useContext(VideoContext);
+  const [url, setUrl] = useState<string>("");
+  const [type, setType] = useState<string>("video");
+
+  const requireVideo = () => {
+    setVideoUrl(url);
+    toast.promise(
+      API.post("video/", { link: url, type }).then((res) => setVideo(res.data)),
+      {
+        pending: "Renderizando o video...",
+        success: "Video renderizado!",
+        error: "Erro ao renderizar!",
+      }
+    );
+  };
+  return (
+    <StyledDivInput>
+      <input
+        type="text"
+        onChange={(e) => setUrl(e.target.value)}
+        placeholder="https://www.youtube.com/watch?v=LMD6MqwErzc&t=1s&ab_channel=JesusAmor"
+      />
+      <select onChange={(e) => setType(e.target.value)}>
+        <option value="video">Video/Audio</option>
+        <option value="audio">Audio</option>
+      </select>
+      <button onClick={requireVideo}>
+        <img src={image} alt="" />
+      </button>
+    </StyledDivInput>
+  );
+}
+export default Input;
